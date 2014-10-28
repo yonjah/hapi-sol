e29d324b271b9a8b6a9e9f293cc8c96a792d0d28
Yoni Jah <yonjah@gmail.com>
2014-10-28 12:38:51 +0800

    Massive breaking changes, improve random SID security and work with hapi 7.0.0

--- examples/server.js
+++ examples/server.js
@@ -1,42 +1,93 @@
-var Hapi = require('hapi');
-var session = require('../');
-var server = new Hapi.Server('127.0.0.1', 8000);
+"use strict";
+var hapi    = require('hapi'),
+	session = require('../'),
+	server  = new hapi.Server(3000);
 
-var config = {
-    password: 'testing',
-    isSecure: false,
-    redirectTo: '/login'
+var users = {
+	john: {
+		id: 'john',
+		password: 'password',
+		name: 'John Doe'
+	}
 };
 
-server.auth('session', {
-    implementation: new session(server, config),
-    defaultMode: true
-});
+var home = function (request, reply) {
+	reply('<html><head><title>Login page</title></head><body><h3>Welcome ' + request.auth.credentials.name + '!</h3><br/><form method="get" action="/logout">' + '<input type="submit" value="Logout">' + '</form></body></html>');
+};
 
-server.route({
-    method: 'get',
-    path: '/login',
-    config: {
-        auth: {
-            mode: 'try'
-        }
-    },
-    handler: function (request) {
-        request.auth.session.set({ user: 'nathan' });
-        request.reply.redirect('/');
-    }
-});
+var login = function (request, reply) {
+	if (request.auth.isAuthenticated) {
+		return reply.redirect('/');
+	}
 
-server.route({
-    method: 'get',
-    path: '/',
-    handler: function (request) {
-        var msg = 'sup, ' + request.auth.credentials.user;
-        request.reply(msg);
-    }
-});
+	var message = '';
+	var account = null;
 
-server.start(function () {
-    console.log('server running at:', server.info.uri);
-});
+	if (request.method === 'post') {
 
+		if (!request.payload.username || !request.payload.password) {
+			message = 'Missing username or password';
+		} else {
+			account = users[request.payload.username];
+			if (!account || account.password !== request.payload.password) {
+				message = 'Invalid username or password';
+			}
+		}
+	}
+
+	if (request.method === 'get' || message) {
+		return reply('<html><head><title>Login page</title></head><body>' + (message ? '<h3>' + message + '</h3><br/>' : '') + '<form method="post" action="/login">' + 'Username: <input type="text" name="username"><br>' + 'Password: <input type="password" name="password"><br/>' + '<input type="submit" value="Login"></form></body></html>');
+	} else {
+		return request.auth.session.set(account, function () {
+			return reply.redirect('/');
+		});
+	}
+};
+
+var logout = function (request, reply) {
+	return request.auth.session.clear(function () {
+		return reply.redirect('/');
+	});
+};
+
+server.pack.register(session, function (err) {
+
+	server.auth.strategy('session', 'session', true, {
+		password: undefined,
+		cookie: 'sid-example',
+		redirectTo: '/login',
+		isSecure: false
+	});
+
+	server.route([{
+		method: 'GET',
+		path: '/',
+		config: {
+			handler: home
+		}
+	}, {
+		method: ['GET', 'POST'],
+		path: '/login',
+		config: {
+			handler: login,
+			auth: {
+				mode: 'try'
+			},
+			plugins: {
+				'session': {
+					redirectTo: false
+				}
+			}
+		}
+	}, {
+		method: 'GET',
+		path: '/logout',
+		config: {
+			handler: logout
+		}
+	}]);
+
+	server.start(function () {
+        console.log('Server ready');
+    });
+});
